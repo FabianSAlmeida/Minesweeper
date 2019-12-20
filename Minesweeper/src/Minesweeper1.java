6aaf22a4f2927c87f26934059e3e09c9d138499c
fabia <fabia@DESKTOP-1BJBD29.home>
2019-12-19 22:54:18 -0500

    Day 1

--- Minesweeper/src/Minesweeper1.java
+++ Minesweeper/src/Minesweeper1.java
@@ -6,7 +6,7 @@ public class Minesweeper1 {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("Hello, \nWorld");
+		System.out.println("Hello World, \nThis is my first day of working on my minesweeper game and today i have setup a way to access my files on and off this desktop");
 		
 		
 	}
